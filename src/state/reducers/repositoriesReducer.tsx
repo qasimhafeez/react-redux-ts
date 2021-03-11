17a55ba5eda1adbeaf6a4bfc2e70136c82bd4a31
Qasim Hafeez <qasimh556@gmail.com>
2021-03-11 15:15:38 +0400

    reducer using TS

--- src/state/reducers/repositoriesReducer.tsx
+++ src/state/reducers/repositoriesReducer.tsx
@@ -4,13 +4,41 @@ interface RepositoriesState {
   data: string[];
 }
 
-const reducer = (state: RepositoriesState, action: any) => {
+interface SearchRepositoriesAction {
+  type: ActionTypes.SEARCH_REPOSITORIES;
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: ActionTypes.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: ActionTypes.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesErrorAction
+  | SearchRepositoriesSuccessAction;
+
+enum ActionTypes {
+  SEARCH_REPOSITORIES = "search_repositories",
+  SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success",
+  SEARCH_REPOSITORIES_ERROR = "search_repositories_error",
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
   switch (action.type) {
-    case "search_repositories":
+    case ActionTypes.SEARCH_REPOSITORIES:
       return { loading: true, error: null, data: [] };
-    case "search_repositories_success":
+    case ActionTypes.SEARCH_REPOSITORIES_SUCCESS:
       return { loading: false, error: null, data: action.payload };
-    case "search_repositories_error":
+    case ActionTypes.SEARCH_REPOSITORIES_ERROR:
       return { loading: false, error: action.payload, data: [] };
     default:
       return state;
