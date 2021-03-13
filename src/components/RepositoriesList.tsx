90298bbe674816968addd136f5f1be75047eb8ff
Qasim Hafeez <qasimh556@gmail.com>
2021-03-13 13:44:23 +0400

    used useTypedSelector ts

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
+  const { loading, error, data } = useTypedSelector(
+    (state) => state.repositories
+  );
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
